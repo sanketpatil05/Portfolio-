b25292ca613dfaea7df9a4f29990ac5dcd52f4c9
Sanket Patil <80532002+sanketpatil05@users.noreply.github.com>
2023-03-14 12:42:26 +0530

    Update ProjectsSection.jsx

--- src/Components/ProjectsSection.jsx
+++ src/Components/ProjectsSection.jsx
@@ -40,20 +40,7 @@ const projects = [
       <SiCss3 className={styles.stackIcon} />,
     ],
   },
-  // {
-  //   name: "Zara.com clone",
-  //   img: "/zara.jpg",
-  //   link: "https://thotapavankumar.github.io/Zara_Project_Clone/",
-  //   git: "https://github.com/Gajanan-Watt/zara_web.git",
-  //   about:
-  //     " It is an e-commerce website having varieties of luxury items which are of high quality. We have tried to clone the pixel perfect website with the help of HTML. Implemented using HTML, CSS and javascript.",
-  //   stacks: [
-  //     
-  //     <SiJavascript className={styles.stackIcon} />,
-  //     <SiHtml5 className={styles.stackIcon} />,
-  //     <SiCss3 className={styles.stackIcon} />,
-  //   ],
-  // },
+
   {
     name: "You Tube App",
     img: "/youtube.png",
@@ -68,24 +55,7 @@ const projects = [
     ],
   }
   
-  // {
-  //   name: "Contio",
-  //   img: "/contio.png",
-  //   link: "https://contio.netlify.app/",
-  //   git: "https://github.com/chandan1499/contio",
-  //   about:
-  //     "Contio is an online portal where anyone can ask there doubts and anyone can become a mentor to help their peers using a built-in chat app",
-
-  //   stacks: [
-  //     <SiReact className={styles.stackIcon} />,
-  //     <SiNodedotjs className={styles.stackIcon} />,
-  //     <SiExpress className={styles.stackIcon} />,
-  //     <SiMongodb className={styles.stackIcon} />,
-  //     <SiJavascript className={styles.stackIcon} />,
-  //     <SiCss3 className={styles.stackIcon} />,
-  //   ],
-  // },
-
+ 
  
 ]
 export function ProjectsSection() {
